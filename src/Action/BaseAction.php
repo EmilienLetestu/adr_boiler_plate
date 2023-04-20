af476bc9d647b6aeab56c72e332eca1cfae10e57
Emilien Letestu <eletestu@gmail.com>
2023-04-20 15:46:52 +0200

    feat: use yaml to define routes

--- src/Action/BaseAction.php
+++ src/Action/BaseAction.php
@@ -11,9 +11,7 @@ namespace App\Action;
 
 use App\Responder\BaseResponder;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/default', name: 'default')]
 class BaseAction
 {
     public function __invoke(BaseResponder $responder): Response
